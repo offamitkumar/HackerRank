275a0d96af2c21de4974cbaccedac3a10f5085ec
Amit Kumar <offamitkumar@gmail.com>
2020-02-26 22:39:00 +0530

    number-group.cpp

---
+++ Maths/number-group.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <vector>
+#include <array>
+#include <set>
+#include <climits>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <bitset>
+#include <numeric>
+using namespace std;
+#define MOD 1000000007
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    long long k; scanf("%lld",&k);
+    printf("%lld",k*k*k);
+    return 0;
+}
