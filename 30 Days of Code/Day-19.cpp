25838beeb5bcd649dcd04461c5655263c1dd5326
Amit Kumar <offamitkumar@gmail.com>
2020-04-28 17:55:19 +0530

    interface , advance-calculator

---
+++ 30 Days of Code/Day-19.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n)=0;
+};
+class Calculator : public AdvancedArithmetic {
+public:
+    int divisorSum(int n) {
+        int sum{};
+        for(int i=1;i<=sqrt(n);++i){
+            if(n%i==0){
+                sum+=i;
+                if(n/i!=i){
+                    sum+=(n/i);
+                }
+            }
+        }
+        return sum;
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    AdvancedArithmetic *myCalculator = new Calculator(); 
+    int sum = myCalculator->divisorSum(n);
+    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    return 0;
+}
