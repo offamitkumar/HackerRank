ef7ab65e0fa43c2ee71d9c3bc617e5391665252c
Amit Kumar <offamitkumar@gmail.com>
2020-05-02 08:20:25 +0530

    simple array processing

---
+++ Interview-Preparation-Kit/Arrays/2D-Array-DS.cpp
@@ -0,0 +1,55 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <vector>
+#include <array>
+#include <set>
+#include <climits>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <bitset>
+#include <numeric>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< // find_by_order & order_of_key
+        int ,
+        null_type ,
+        less<int> ,
+        rb_tree_tag ,
+        tree_order_statistics_node_update 
+> new_set;
+#define MOD 1000000007
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    int arr[6][6];
+    for(int i=0;i<6;++i){
+        for(int j=0;j<6;++j){
+            cin>>arr[i][j];
+        }
+    }
+    int ans{INT_MIN};
+    for(int i=1;i<5;++i){
+        for(int j=1;j<5;++j){
+            int sum = arr[i-1][j-1]+arr[i-1][j]+arr[i-1][j+1]+arr[i][j]+arr[i+1][j-1]+arr[i+1][j]+arr[i+1][j+1];
+            ans = max(sum,ans);
+        }
+    }
+    cout<<ans<<'\n';
+    return 0;
+}
