c8bf5efe1d605b1299c8da7425602f724660ae38
Amit Kumar <offamitkumar@gmail.com>
2020-05-06 12:08:45 +0530

    head , seq  command

---
+++ Shell Scripting/Head_of_a_Text_File#1.sh
@@ -0,0 +1,8 @@
+# all of the below command will give same output & can be modified best answer will be using seq or head directly 
+
+
+
+#sed 20q < /dev/stdin
+#head -20 < /dev/stdin
+#head -20 $1
+cat | head -20
