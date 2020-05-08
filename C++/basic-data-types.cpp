7ce40388566f2eb818fc6b38358d567364ffe56f
Amit Kumar <offamitkumar@gmail.com>
2020-05-08 21:49:27 +0530

    data types

---
+++ C++/basic-data-types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a; long long b; char c; float d; double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl<<b<<endl<<c<<endl;
+    cout<<fixed<<setprecision(3)<<d<<endl;
+    cout<<fixed<<setprecision(9)<<e;
+
+    return 0;
+}
