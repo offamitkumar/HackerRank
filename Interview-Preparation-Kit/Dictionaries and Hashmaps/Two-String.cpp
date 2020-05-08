14f105ea84717d4e03f0afe16501163a48066c8b
Amit Kumar <offamitkumar@gmail.com>
2020-05-08 15:01:42 +0530

    check existing substring

---
+++ Interview-Preparation-Kit/Dictionaries and Hashmaps/Two-String.cpp
@@ -0,0 +1,59 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <vector>
+#include <array>
+#include <set>
+#include <climits>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <bitset>
+#include <numeric>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< // find_by_order & order_of_key
+        int ,
+        null_type ,
+        less<int> ,
+        rb_tree_tag ,
+        tree_order_statistics_node_update 
+> new_set;
+#define MOD 1000000007
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    int t; scanf("%d",&t);
+    while(t--){
+         string a,b;
+         cin>>a>>b;
+         set<char>s;
+         for(auto&itr:a){
+             s.insert(itr);
+         }
+         for(auto&itr:b){
+             if(s.find(itr)!=s.end()){
+                 cout<<"YES"<<'\n';
+                 goto hell;
+             }
+         }
+         cout<<"NO"<<'\n';
+hell:
+         ;
+    }
+    return 0;
+}
