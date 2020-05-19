518b1bb6bede0bf1bda1a46ded02e759b8ff6a92
Amit Kumar <offamitkumar@gmail.com>
2020-05-19 12:25:42 +0530

    hashing , searching , simple

---
+++ Interview-Preparation-Kit/Search/Hash Tables: Ice Cream Parlor.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the whatFlavors function below.
+void whatFlavors(vector<int> cost, int money) {
+    map<int , int>hashTable; // value -> index
+    const size_t n = cost.size();
+    for(int index = 0;index < static_cast<int>(n); ++index ){
+        int &&remainingPrice = money - cost[index];
+        if(remainingPrice > 0 and hashTable.find(remainingPrice)!=hashTable.end()){
+            const int &indexOne = hashTable[remainingPrice];
+            const int &indexTwo = index;
+            if(indexOne < indexTwo){
+                cout<<indexOne+1<<' '<<indexTwo+1<<'\n';
+            }else{
+                cout<<indexTwo+1<<' '<<indexOne+1<<'\n';
+            }
+        }
+        hashTable[ cost[ index ] ] = index;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int money;
+        cin >> money;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string cost_temp_temp;
+        getline(cin, cost_temp_temp);
+
+        vector<string> cost_temp = split_string(cost_temp_temp);
+
+        vector<int> cost(n);
+
+        for (int i = 0; i < n; i++) {
+            int cost_item = stoi(cost_temp[i]);
+
+            cost[i] = cost_item;
+        }
+
+        whatFlavors(cost, money);
+    }
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
