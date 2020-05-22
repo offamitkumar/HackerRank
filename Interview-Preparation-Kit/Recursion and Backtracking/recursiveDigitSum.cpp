0aef0c938a26c66dbc7af237b378de31e41d8e5a
Amit Kumar <offamitkumar@gmail.com>
2020-05-22 17:14:56 +0530

    recursive digit sum

---
+++ Interview-Preparation-Kit/Recursion and Backtracking/recursiveDigitSum.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the superDigit function below.
+int superDigit(string n, int k) {
+    int64_t sum{};
+    for_each(n.begin() , n.end() , [&](char c){
+            sum = (sum + (c - '0'))%9;
+            });
+    sum%=9;
+    k%=9;
+    sum = (sum*k)%9;
+    return (sum)?sum:9;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nk_temp;
+    getline(cin, nk_temp);
+
+    vector<string> nk = split_string(nk_temp);
+
+    string n = nk[0];
+
+    int k = stoi(nk[1]);
+
+    int result = superDigit(n, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
