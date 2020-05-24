b7d2ab64c06f392d363d3dd0228fe0105c3864a4
Amit Kumar <offamitkumar@gmail.com>
2020-05-24 16:28:29 +0530

    added both O(1) and O(N) solution

--- Algorithms/Game Theory/Game of Stones.cpp
+++ Algorithms/Game Theory/Game of Stones.cpp
@@ -3,11 +3,43 @@
 using namespace std;
 
 // Complete the gameOfStones function below.
+vector<bool>dp(110); // tells whether current position is loosing or winning
+bool isCalculatedBefore = false;
+void preCalculate(void) {
+    isCalculatedBefore = true;
+    dp[0]=0;
+    dp[1]=0;
+    dp[2]=1;
+    dp[3]=1;
+    dp[4]=1; // give 3 and reach 1 
+    dp[5]=1;
+    for( int i=6;i<=101;++i ){
+        if((!dp[i-2])||(!dp[i-5])||(!dp[i-3])){
+            dp[i]=1;
+        }else{
+            dp[i]=0;
+        }
+    }
+}
 string gameOfStones(int n) {
+    /*
+     * O(1)
+     *
+     *
+
     if(n%7==0 || n%7==1){
         return "Second";
     }else{
         return "First";
+    }
+
+
+    */
+    preCalculate();
+    if(dp[n]){
+        return "First";
+    }else{
+        return "Second";
     }
 }
 
