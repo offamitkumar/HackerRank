844bc10dd4ac76ddba3f2f947c8d6bbab6085214
Amit Kumar <offamitkumar@gmail.com>
2020-05-30 14:43:20 +0530

    added card doesn't change game state, as other remove same number of cards

---
+++ Algorithms/Game Theory/Poker Nim.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the pokerNim function below.
+string pokerNim(int k, vector<int> c) {
+    int state = 0;
+    for (auto & itr : c) {
+        state ^= itr;
+    }
+    return state ? "First" : "Second";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string nk_temp;
+        getline(cin, nk_temp);
+
+        vector<string> nk = split_string(nk_temp);
+
+        int n = stoi(nk[0]);
+
+        int k = stoi(nk[1]);
+
+        string c_temp_temp;
+        getline(cin, c_temp_temp);
+
+        vector<string> c_temp = split_string(c_temp_temp);
+
+        vector<int> c(n);
+
+        for (int i = 0; i < n; i++) {
+            int c_item = stoi(c_temp[i]);
+
+            c[i] = c_item;
+        }
+
+        string result = pokerNim(k, c);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
