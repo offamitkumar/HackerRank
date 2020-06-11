bcd44e18fe25be5c73b119c9c77addd08e1d2ca4
Amit Kumar <offamitkumar@gmail.com>
2020-06-12 00:31:37 +0530

    just sort and process

---
+++ Algorithms/Search/Hackerland Radio Transmitters.cpp
@@ -0,0 +1,85 @@
+//  Author : Amit Kumar 
+
+//  #include <ext/pb_ds/assoc_container.hpp>
+//  #include <ext/pb_ds/tree_policy.hpp>
+
+//  #include <iostream>
+  #include <algorithm>
+//  #include <string>
+//  #include <sstream>
+//  #include <fstream>
+//  #include <iomanip>
+//  #include <chrono>
+//  #include <numeric>
+//  #include <utility>
+
+//  #include <bitset>
+//  #include <tuple>
+//  #include <queue>
+//  #include <stack>
+//  #include <vector>
+  #include <array>
+//  #include <unordered_map>
+//  #include <unordered_set>
+//  #include <set>
+//  #include <map>
+//  #include <deque>
+
+//  #include <climits>
+//  #include <cstring>
+//  #include <cmath>
+//  #include <cassert>
+#include <cstdio>
+using namespace std;
+//  using namespace std::chrono;
+//  using namespace placeholders;
+//  using namespace __gnu_pbds;
+//  template<typename TypeInfo>
+//  using new_set = tree< // find_by_order & order_of_key
+//          TypeInfo ,
+//          null_type ,
+//          less<TypeInfo> ,
+//          rb_tree_tag ,
+//          tree_order_statistics_node_update 
+//  > ;
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e5+100;
+const int MOD = (int)1e9+7;
+array<int , MAXN>arr;
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+//    auto initial_time = high_resolution_clock::now();
+    int n , k; scanf("%d %d",&n,&k);
+    for(int i=0;i<n;++i){
+        scanf("%d",&arr[i]);
+    }
+    sort(arr.begin() , arr.begin()+n);
+    int counter{};
+    for(int i=0;i<n;++i){
+        int current_pos = arr[i];
+        int j;
+        for(j=i+1;j<n;++j){
+            if(arr[j]-arr[i] <= k){
+                current_pos =arr[j];
+            }else{
+                break;
+            }
+        }
+        i = j-1;
+        ++counter;
+        while(i<n && arr[i]-current_pos <=k){
+            ++i;
+        }
+        if(i!=n){
+            --i;
+        }
+    }
+    printf("%d\n",counter);
+//    auto final_time = high_resolution_clock::now();
+//    cout << duration_cast<milliseconds>(final_time-initial_time).count() << " ms" << '\n'; 
+    return 0;
+}
