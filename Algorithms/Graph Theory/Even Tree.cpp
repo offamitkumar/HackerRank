0c77388034ac31e831a91bc820eb688e98ad4427
Amit Kumar <offamitkumar@gmail.com>
2020-06-13 16:16:46 +0530

    simple dfs

---
+++ Algorithms/Graph Theory/Even Tree.cpp
@@ -0,0 +1,84 @@
+//  Author : Amit Kumar 
+
+//  #include <ext/pb_ds/assoc_container.hpp>
+//  #include <ext/pb_ds/tree_policy.hpp>
+
+//  #include <iostream>
+//  #include <algorithm>
+//  #include <string>
+//  #include <sstream>
+//  #include <fstream>
+//  #include <iomanip>
+//  #include <chrono>
+//  #include <numeric>
+//  #include <utility>
+
+//  #include <bitset>
+//  #include <tuple>
+//  #include <queue>
+//  #include <stack>
+  #include <vector>
+//  #include <array>
+//  #include <unordered_map>
+//  #include <unordered_set>
+//  #include <set>
+//  #include <map>
+//  #include <deque>
+
+//  #include <climits>
+//  #include <cstring>
+//  #include <cmath>
+//  #include <cassert>
+#include <cstdio>
+using namespace std;
+//  using namespace std::chrono;
+//  using namespace placeholders;
+//  using namespace __gnu_pbds;
+//  template<typename TypeInfo>
+//  using new_set = tree< // find_by_order & order_of_key
+//          TypeInfo ,
+//          null_type ,
+//          less<TypeInfo> ,
+//          rb_tree_tag ,
+//          tree_order_statistics_node_update 
+//  > ;
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e5+100;
+const int MOD = (int)1e9+7;
+vector<vector<int>>graph;
+int ans{};
+int dfs(int current_node , int parent){
+    int child_count{};
+    for(auto&itr:graph[current_node]){
+        int num{};
+        if(itr != parent){
+            num = dfs(itr , current_node);
+        }
+        if(num && (num%2==0)){
+            ++ans;
+        }else{
+            child_count+=num;
+        }
+    }
+    return child_count+1;
+}
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+//    auto initial_time = high_resolution_clock::now();
+    int n,m; scanf("%d %d",&n,&m);
+    graph.resize(n+1);
+    for(int i=0;i<m;++i){
+        int u , v; scanf("%d %d",&u,&v);
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    dfs(1,-1);
+    printf("%d\n",ans);
+//    auto final_time = high_resolution_clock::now();
+//    cout << duration_cast<milliseconds>(final_time-initial_time).count() << " ms" << '\n'; 
+    return 0;
+}
