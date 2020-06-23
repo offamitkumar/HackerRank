b27467c5d9d1e61e56a8b47d136e5b424b8710b3
Amit Kumar <offamitkumar@gmail.com>
2020-06-23 10:10:46 +0530

    simple

---
+++ Algorithms/Greedy/Largest Permutation.cpp
@@ -0,0 +1,151 @@
+//  Author : Amit Kumar 
+
+#include <iostream>
+#include <algorithm> 
+#include <string> 
+#include <sstream>
+#include <fstream>
+#include <iomanip> 
+#include <chrono> 
+#include <numeric> 
+#include <utility>
+#include <functional>
+
+#include <bitset>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <deque>
+#include <list>
+
+
+#include <climits>
+#include <cstring>
+#include <cmath>
+#include <cassert>
+#include <cstdio>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace std::chrono;
+//  using namespace placeholders;
+using namespace __gnu_pbds;
+template<typename TypeInfo>
+using new_set = tree< 
+/* ob.order_of_key(element) -> number of element strictly less than element
+ * *ob.find_by_order(n) ->  nth element in increasing order
+ */
+         TypeInfo ,
+         null_type ,
+         less<TypeInfo> ,
+         rb_tree_tag ,
+         tree_order_statistics_node_update 
+ > ;
+
+void show_me() { cerr << endl; }
+ 
+template <typename HEAD, typename... TAIL>
+void show_me(HEAD H, TAIL... T) {
+  cerr << " " << (H);
+  show_me(T...);
+}
+ 
+#ifdef HELL_JUDGE
+#define show(...) cerr << "[" << #__VA_ARGS__ << "]:", show_me(__VA_ARGS__)
+#else
+#define show(...) 0
+#endif
+ 
+template<typename A, typename B>
+ostream& operator<<(ostream&out, const pair<A, B>&p){
+    out << p.first << ' ' << p.second ;
+    return out;
+}
+
+template<typename A, typename B>
+istream& operator>>(istream&in, pair<A, B>&p){
+    in >> p.first >> p.second ;
+    return in;
+}
+
+template<typename A> 
+istream& operator>>(istream&in, vector<A>&vec){
+    for(auto&itr:vec){
+        in >> itr;
+    }
+    return in;
+}
+
+template<typename A>
+ostream& operator<<(ostream&out,const vector<A>&vec){
+    for(auto&itr:vec){
+        out << itr <<' '; // VVV 
+    }
+    return out;
+}
+
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e5+100;
+const int MOD  = (int)1e9+7;
+
+
+
+
+signed main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    ios::sync_with_stdio(false); // FLUSH THE STREAM IF USING puts / printf / scanf/
+    cin.tie(nullptr);            //
+    cout.tie(nullptr);           //
+#ifdef HELL_JUDGE
+    auto INITIAL_TIME = high_resolution_clock::now();
+#endif 
+
+    int n; cin >> n;
+    int k; cin >> k;
+    vector<int>v(n+1);
+    vector<int>pos(n+1);
+    for(int i=1;i<=n;++i){
+        cin >> v[i];
+        pos[v[i]] = i;
+    }
+    for(int i=0;i<n && k; ++i){
+        if(v[i+1]==n-i){
+            continue;
+        }
+        int loc = pos[n-i];
+        pos[v[i+1]] = pos[n-i];
+        pos[n-i] = i+1;
+        swap(v[i+1],v[loc]);
+        --k;
+    }
+//    k%=2;
+//    while(k){
+//        swap(v[n-1],v[n]);
+//        --k;
+//    }
+//    
+    for(int i=1;i<=n;++i){
+        cout << v[i]<<' ';
+    }
+    cout << '\n'; 
+
+#ifdef HELL_JUDGE
+    auto FINAL_TIME = high_resolution_clock::now();
+    cout << "Time : "
+         << duration_cast<milliseconds>(FINAL_TIME-INITIAL_TIME).count() 
+         << " ms" << '\n'; 
+#endif 
+    return 0;
+}
