0e93bbf2e9cbcd9fbae6315f11c1832c1cf245c2
Amit Kumar <offamitkumar@gmail.com>
2020-06-26 11:58:42 +0530

    max range for pole to keep current point in range

---
+++ Algorithms/Greedy/Goodland Electricity.cpp
@@ -0,0 +1,146 @@
+//  Author : Amit Kumar 
+
+#include <iostream>
+#include <algorithm> 
+#include <string> 
+#include <sstream>
+#include <fstream>
+#include <iomanip> 
+#include <chrono> 
+#include <numeric> 
+#include <utility>
+#include <functional>
+
+#include <bitset>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <deque>
+#include <list>
+
+
+#include <climits>
+#include <cstring>
+#include <cmath>
+#include <cassert>
+#include <cstdio>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace std::chrono;
+//  using namespace placeholders;
+using namespace __gnu_pbds;
+template<typename TypeInfo>
+using new_set = tree< 
+/* ob.order_of_key(element) -> number of element strictly less than element
+ * *ob.find_by_order(n) ->  nth element in increasing order
+ */
+         TypeInfo ,
+         null_type ,
+         less<TypeInfo> ,
+         rb_tree_tag ,
+         tree_order_statistics_node_update 
+ > ;
+
+void show_me() { cerr << endl; }
+ 
+template <typename HEAD, typename... TAIL>
+void show_me(HEAD H, TAIL... T) {
+  cerr << " " << (H);
+  show_me(T...);
+}
+ 
+#ifdef HELL_JUDGE
+#define show(...) cerr << "[" << #__VA_ARGS__ << "]:", show_me(__VA_ARGS__)
+#else
+#define show(...) 0
+#endif
+
+typedef int64_t Int;
+ 
+template<typename A, typename B>
+ostream& operator<<(ostream&out, const pair<A, B>&p){
+    out << p.first << ' ' << p.second ;
+    return out;
+}
+
+template<typename A, typename B>
+istream& operator>>(istream&in, pair<A, B>&p){
+    in >> p.first >> p.second ;
+    return in;
+}
+
+template<typename A> 
+istream& operator>>(istream&in, vector<A>&vec){
+    for(auto&itr:vec){
+        in >> itr;
+    }
+    return in;
+}
+
+template<typename A>
+ostream& operator<<(ostream&out,const vector<A>&vec){
+    for(auto&itr:vec){
+        out << itr <<' '; // VVV 
+    }
+    return out;
+}
+
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e5+100;
+const int64_t MOD  = (int64_t)1e9+7ll;
+
+
+
+
+signed main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    ios::sync_with_stdio(false); // FLUSH THE STREAM IF USING puts / printf / scanf/
+    cin.tie(nullptr);            //
+    cout.tie(nullptr);           //
+#ifdef HELL_JUDGE
+    auto INITIAL_TIME = high_resolution_clock::now();
+#endif 
+
+    int n , k; cin >> n >> k; 
+    vector<int>v(n); cin >> v; 
+    int last_pole = -1;
+    int total = 0;
+    for(int i=0;i<n;){
+        int j = min(i+k-1 , n-1);
+        while(last_pole <= j ){
+            if(v[j] ==1){
+                break;
+            }
+            --j;
+        }
+        if(j < last_pole){
+            cout <<  -1 << endl; 
+            return 0;
+        }
+        last_pole = j+1;
+        total += 1;
+        i = j+k;
+    }
+    cout << total << '\n'; 
+
+#ifdef HELL_JUDGE
+    auto FINAL_TIME = high_resolution_clock::now();
+    cout << "Time : "
+         << duration_cast<milliseconds>(FINAL_TIME-INITIAL_TIME).count() 
+         << " ms" << '\n'; 
+#endif 
+    return 0;
+}
