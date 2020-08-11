dd41f6f1068b6d3ac6847f7e48c681c50f1af8a1
Amit Kumar <offamitkumar@gmail.com>
2020-08-11 12:22:09 +0530

    basic observation

---
+++ 10 Days of Statistics/Day 2: Basic Probability.go
@@ -0,0 +1,6 @@
+// 2 dice have 6 * 6 outcomes and out of those if we count those who have sum less then 9 they 
+// will be 30 {should have counted those who have sum greater then 9 ;)} 
+// P(A) = favourable_outcome / total_outcome 
+//  = > 30 / 36  
+//  = > 5 / 6 
+// So option B is correct.
