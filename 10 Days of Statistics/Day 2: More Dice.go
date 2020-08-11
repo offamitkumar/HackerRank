0c21432c71d5ed28f398f09e8ab80c917fc7f255
Amit Kumar <offamitkumar@gmail.com>
2020-08-11 12:27:58 +0530

    conditional probablity

---
+++ 10 Days of Statistics/Day 2: More Dice.go
@@ -0,0 +1,8 @@
+// 1. Values should be different so (1,1) (2,2) (3,3) .... (6,6) will be excluded, after it
+//      remaining favourable cases 30 
+// 2. Now we have to look for those values whose sum is 6 then (1,5) , (2,4) , (4,2) , (5, 1) are remaining. 
+// 
+
+// So P = 4  / 36  -> 1/9
+
+// Option A is correct. 
