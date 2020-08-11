bd276e06d25b7a71e21c8bb4764d73adb1371c51
Amit Kumar <offamitkumar@gmail.com>
2020-08-11 12:40:00 +0530

    basic observation

---
+++ 10 Days of Statistics/Day 2: Compound Event Probability.go
@@ -0,0 +1,11 @@
+// possible cases are that 
+// black ball from 1 and red ball from other two : 3/7 * 5/9 * 1/2
+// black ball from 2 and red ball from other two : 4/9 * 4/7 * 1/2
+// black ball from 3 and red ball from other two : 1/2 * 5/9 * 4/7
+
+// -> 15/126 + 16/126 + 20/126 
+// -> 51 / 126 
+
+// gcd will be 3 so dividing both num & deno by 3 we get
+// -> 17/42 
+// So option C is correct
